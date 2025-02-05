0e4a7d3f1a396cf968199f7b500677145bc8d244
Ahasn-9 <sp21-bse-024@cuilahore.edu.pk>
2025-02-05 13:16:00 +0500

    Error Handling

--- app.js
+++ app.js
@@ -1,38 +1,13 @@
-const sections = document.querySelectorAll(".section");
-const sectionbtns = document.querySelectorAll(".controls");
-const sectionbtn = document.querySelectorAll(".control");
-const allSections = document.querySelector(".main-container");
-
-function pageTransition() {
-  // Button click active class
-  for (i = 0; i < sectionbtn.length; i++) {
-    sectionbtn[i].addEventListener("click", function () {
-      let currentBtn = document.querySelector(".active-btn");
-      currentBtn.className = currentBtn.className.replace(" active-btn", "");
-      this.className += " active-btn";
+(function () {
+  [...document.querySelectorAll(".control")].forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
     });
-  }
-  // active section
-  allSections.addEventListener("click", (e) => {
-    const id = e.target.dataset.id;
-    if (id) {
-      sectionbtns.forEach((btn) => {
-        btn.classList.remove("active");
-      });
-      e.target.classList.add("active");
-      //hide other sections
-      sections.forEach((section) => {
-        section.classList.remove("active");
-      });
-      const element = document.getElementById(id);
-      element.classList.add("active");
-    }
   });
-  // togle theme
-  const themebtn = document.querySelector(".theme-btn");
-  themebtn.addEventListener("click", () => {
-    let element = document.body;
-    element.classList.toggle("light-mode");
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
   });
-}
-pageTransition();
+})();
